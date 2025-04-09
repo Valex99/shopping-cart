cb008efa25a329d6970b5a219052e8410a20913b
valex <val.matej99@gmail.com>
2025-04-09 17:06:23 +0200

    FeaturedCollection page completed

---
+++ src/components/featured-collection/FeaturedCollection.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import image1 from "./collection-images/image1.jpg";
+import image2 from "./collection-images/image2.jpg";
+
+export default function FeaturedCollection() {
+  return (
+    <div className="flex p-5 py-15 px-5 w-full">
+      <div className="flex flex-col w-2/5 py-16 px-6 gap-[15px]">
+        <h2 className="text-2xl font-bold tracking-wide">SERAPHÉ</h2>
+        <p>
+          “SERAPHÉ embodies refined craftsmanship, where precision engineering
+          meets timeless elegance. With luxurious materials and a design that
+          emphasizes understated beauty, it’s a collection for those who
+          appreciate both performance and quiet sophistication.”
+        </p>
+        <Link to="products" className="underline font-bold text-gray-800">
+          BROWSE “SERAPHÉ”
+        </Link>
+        <img src={image1} alt="" />
+      </div>
+
+      <div className="flex flex-col w-3/5 py-46 gap-[15px]">
+        <img src={image2} alt="" />
+        <button className="w-3/8 py-3 px-6 mt-6 bg-black text-white font-bold hover:bg-gray-800 transition-all cursor-pointer">
+          SEE THE COLLECTION
+        </button>
+      </div>
+    </div>
+  );
+}
