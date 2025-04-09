868b58cc4b0f45d725071cc608fe7edc0bffcf6b
valex <val.matej99@gmail.com>
2025-04-09 12:47:49 +0200

    Carousel version 2 complete

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+import { ArrowBigLeft, ArrowBigRight, CircleDot, Circle } from "lucide-react";
 // Classic image carousel
 // Should contain some text (collection - description)
 // Should contain button shop now -> leads to all products page
@@ -16,38 +16,56 @@ import watch6 from "./carousel-images/watch6.jpg";
 const images = [watch1, watch2, watch3, watch4, watch5, watch6];
 
 export default function Carousel() {
+  const showNextImage = () => {
+    setImageIndex((index) => {
+      // Check if we are at the end - wrap aorund
+      if (index === images.length - 1) return 0;
+      return index + 1;
+    });
+  };
 
-
-
- const showNextImage = () => {
-    setImageIndex(index => {
-        // Check if we are at the end - wrap aorund
-        if (index === images.length - 1) return 0
-        return index + 1
-    })
- }
-
-
- const showPrevImage = () => {
-    setImageIndex(index => {
-        if (index === 0) return images.length -1
-        return index -1
-    })
- }
-
+  const showPrevImage = () => {
+    setImageIndex((index) => {
+      if (index === 0) return images.length - 1;
+      return index - 1;
+    });
+  };
 
   // By default index is zero
   const [imageIndex, setImageIndex] = useState(0);
   return (
     <div className="relative max-h-[650px] w-full overflow-hidden">
-      <img src={images[imageIndex]} className="w-full h-full object-cover" />
+      {/* Show all images - map through array and translate each one by 100% + add animation*/}
+      <div className="w-full h-full overflow-hidden flex">
+        {images.map((url) => (
+          <img
+            key={url}
+            src={url}
+            className="img object-cover flex-shrink-0 flex-grow-0"
+            style={{ translate: `${-100 * imageIndex}%` }}
+          />
+        ))}
+      </div>
       <button className="slider-btn" onClick={showPrevImage}>
         <ArrowBigLeft />
       </button>
       <button className="slider-btn right-0" onClick={showNextImage}>
         <ArrowBigRight />
       </button>
+
+      {/* // Buttons / image pointers */}
+      <div className="absolute bottom-3 left-1/2 -translate-x-1/2 flex gap-1">
+        {" "}
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className="carousel-slider-dot-btn"
+            onClick={() => setImageIndex(index)}
+          >
+            {index === imageIndex ? <CircleDot /> : <Circle />}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
- 
