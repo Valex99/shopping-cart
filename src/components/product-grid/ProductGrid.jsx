4cd5cf76fbb04827e811f5dd36a125cce862bed9
valex <val.matej99@gmail.com>
2025-04-11 18:28:10 +0200

    Working on grid items

--- src/components/product-grid/ProductGrid.jsx
+++ src/components/product-grid/ProductGrid.jsx
@@ -10,8 +10,16 @@ export default function ProductGrid() {
   // .then(console.log);
 
   return (
-    <div className="flex px-40 py-20 gap-20">
-      <div className="w-[100px] bg-black"></div>
+    <div
+      className="flex px-40 py-20 gap-20 
+    flex justify-center
+    "
+    >
+      <div className="w-[200px] bg-black text-white">
+        {" "}
+        Maybe add me as a position absolute so i am taken out of the document
+        flow
+      </div>
 
       <div className="w-full grid-container">
         {/* // Grid childs */}
@@ -30,6 +38,8 @@ export default function ProductGrid() {
         <ProductItem imgSrc={prx} itemName="TISSOT PRX" itemPrice="649€" />
         <ProductItem imgSrc={prx} itemName="TISSOT PRX" itemPrice="649€" />
         <ProductItem imgSrc={prx} itemName="TISSOT PRX" itemPrice="649€" />
+
+        {/* // ADD PAGES - BUTTONS + NUMBERS */}
       </div>
     </div>
   );
