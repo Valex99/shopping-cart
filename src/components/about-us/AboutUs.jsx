cfec2cefaf051dc7debbde8485509b319c41527c
valex <val.matej99@gmail.com>
2025-04-18 01:13:28 +0200

    Working on About Us page

---
+++ src/components/about-us/AboutUs.jsx
@@ -0,0 +1,5 @@
+export default function AboutUs() {
+return <div>
+    <h1>Hello from About Us page</h1>
+</div>
+}
---
+++ src/components/about-us/about.js
@@ -0,0 +1,10 @@
+import banncerPic from "../banner/banner-image/time.jpg";
+
+const aboutUsData = {
+  title: "Our Commitment to Timeless Quality & Care",
+  description:
+    "We believe that everyone deserves access to high-quality, reliable timepieces. That’s why we’ve partnered with select organizations to promote craftsmanship, heritage, and sustainability in watchmaking.With every purchase, we aim to not only deliver a timeless accessory but also to educate and inspire our customers on how to care for and preserve their watches for years to come. From maintenance tips to style guides, we’re here to help you make the most of your time.",
+  image: banncerPic,
+};
+
+export default aboutUsData;
