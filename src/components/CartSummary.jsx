37aab6ee476ebeda9de0a4d9b7679bea18965a99
valex <val.matej99@gmail.com>
2025-04-17 19:57:23 +0200

    Checkout process and cart completed

---
+++ src/components/CartSummary.jsx
@@ -0,0 +1,29 @@
+export default function CartSummary({ totalPrice, onClick }) {
+  return (
+    <div className="w-full md:w-96 mt-10 md:mt-0 md:ml-auto p-6 pr-0 pt-10 rounded-md bg-white ">
+      <h2 className="text-2xl font-bold mb-6">Order Summary</h2>
+
+      <div className="flex justify-between items-center mb-4">
+        <span className="text-gray-700">Subtotal</span>
+        <span className="font-medium text-gray-900">€ {totalPrice}</span>
+      </div>
+
+      <div className="flex justify-between items-center mb-4 text-sm ">
+        <span className="text-gray-700">Shipping</span>
+        <span className="text-gray-700">Free</span>
+      </div>
+
+      <div className="flex justify-between items-center mb-6 text-sm border-b border-gray-400 pb-4">
+        <span className="text-gray-700">Taxes</span>
+        <span className="text-gray-700">Calculated at checkout</span>
+      </div>
+
+      <button
+        className="w-full bg-black text-white text-sm font-medium py-3 rounded hover:opacity-90 active:scale-95 transition transform duration-200 cursor-pointer"
+        onClick={onClick}
+      >
+        Checkout
+      </button>
+    </div>
+  );
+}
--- src/components/cart-item/CartItem.jsx
+++ src/components/cart-item/CartItem.jsx
@@ -5,55 +5,60 @@ import "./cart.css";
 export default function CartItem({ id, img, name, price, quantity }) {
   const { removeFromCart, updateQuantity } = useCart();
   return (
-    <div className="grid grid-cols-6 items-center py-6 text-gray-800 text-sm border-b">
-      {/* Item section */}
+      <div className="grid grid-cols-6 items-center py-6 text-gray-800 text-sm border-b">
+        {/* Item section */}
 
-      <div className="col-span-2 flex items-center gap-4">
-        <img
-          src={img}
-          alt={name}
-          className="hover:scale-102 transition-transform duration-500 border border-[#c87c2e] ease-in-out w-20 h-20 object-cover rounded cursor-pointer"
-        />
-        <h2 className="font-bold">{name}</h2>
-      </div>
+        <div className="col-span-2 flex items-center gap-4">
+          <img
+            src={img}
+            alt={name}
+            className="hover:scale-102 transition-transform duration-500 border border-[#c87c2e] ease-in-out w-20 h-20 object-cover rounded cursor-pointer"
+          />
+          <h2 className="font-bold">{name}</h2>
+        </div>
 
-      {/* Price */}
-      <div className="text-left font-medium">€ {price.toFixed(2)}</div>
+        {/* Price */}
+        <div className="text-left font-bold">€ {price.toFixed(2)}</div>
 
-      {/* Quantity */}
-      <div className="quantity-container">
-        <button
-          className="decrease"
-          onClick={() => updateQuantity(id, quantity - 1)}
-          disabled={quantity === 1}
-        >
-          -
-        </button>
-        <input type="text" className="amount input" value={quantity} min="1" />
-        <button
-          className="increase"
-          onClick={() => updateQuantity(id, quantity + 1)}
-        >
-          +
-        </button>
-      </div>
+        {/* Quantity */}
+        <div className="quantity-container">
+          <button
+            className="decrease"
+            onClick={() => updateQuantity(id, quantity - 1)}
+            disabled={quantity === 1}
+          >
+            -
+          </button>
+          <input
+            type="text"
+            className="amount input"
+            value={quantity}
+            min="1"
+          />
+          <button
+            className="increase"
+            onClick={() => updateQuantity(id, quantity + 1)}
+          >
+            +
+          </button>
+        </div>
 
-      {/* Total */}
-      <div className="text-left font-semibold">
-        € {(price * quantity).toFixed(2)}
-      </div>
+        {/* Total */}
+        <div className="text-left font-semibold">
+          € {(price * quantity).toFixed(2)}
+        </div>
 
-      {/* Remove Button */}
-      <div>
-        <button
-          className="underline text-black px-4 py-2 rounded bg-gray-200 hover:bg-[#423c3a] hover:text-white transition font-bold cursor-pointer
+        {/* Remove Button */}
+        <div>
+          <button
+            className="underline text-black px-4 py-2 rounded bg-gray-200 hover:bg-[#423c3a] hover:text-white transition font-bold cursor-pointer
         "
-          // Very simple solution - just pass id to current product - work off ids
-          onClick={() => removeFromCart(id)}
-        >
-          Remove
-        </button>
-      </div>
+            // Very simple solution - just pass id to current product - work off ids
+            onClick={() => removeFromCart(id)}
+          >
+            Remove
+          </button>
+        </div>
     </div>
   );
 }
