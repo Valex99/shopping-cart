821d41e88442c2c82cdcb347fc70f848a59d75ef
valex <val.matej99@gmail.com>
2025-04-18 15:08:33 +0200

    Sticky header properly working

--- src/components/HeaderWrapper.jsx
+++ src/components/HeaderWrapper.jsx
@@ -3,30 +3,38 @@ import Header from "./Header";
 import StickyHeader from "./StickyHeader";
 
 export default function HeaderWrapper() {
-  // Set boolean state on which component to show
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const currentScroll = window.scrollY;
-
-      // Add a buffer zone: Only toggle to StickyHeader after scrolling down by more than 130px
-      // and only switch back to Header when the scroll is less than 100px.
-      if (currentScroll > 130 && !isSticky) {
-        setIsSticky(true); // Show StickyHeader when scroll is more than 130px
-      } else if (currentScroll < 100 && isSticky) {
-        setIsSticky(false); // Show Header when scroll is less than 100px
+      if (window.scrollY > 120) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
       }
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isSticky]);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      {!isSticky && <Header />}
-      {isSticky && <StickyHeader />}
+      <div>
+        {/* The main header is always visible */}
+        <Header />
+        {/* The sticky header will only be visible once scrolled past 120px */}
+        <div
+          className={`transition-all duration-300 ease-in-out ${
+            isSticky ? "fixed top-0 left-0 w-full z-50" : "hidden"
+          }`}
+        >
+          <StickyHeader />
+        </div>
+      </div>
     </>
   );
 }
